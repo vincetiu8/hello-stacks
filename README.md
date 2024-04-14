cbd5efa089038761f820ce33dddd8ea5b15507c7
Vincent Tiu <vincetiu8@gmail.com>
2024-04-14 07:40:31 -0400

    add frontend

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Hello Stacks
 
-Basic dApp for stacks demo.
+gi
 
 This is a basic dApp to demonstrate how to build on stacks.
 
@@ -12,4 +12,46 @@ Contracts holds all of the clarity contracts that are published to stacks and th
 
 ##### `./contracts/hello-stacks.clar`
 
-This is our clarity smart contract 
+This is our clarity smart contract that we will publish to the chain and interact with from the frontend.
+
+To test:
+1. Install [Clarinet](https://github.com/hirosystems/clarinet)
+2. `cd contracts`
+3. `clarinet console`
+    1. In the console: `(contract-call? .hello-stacks write-message u"Hello there")`
+
+##### Running devnet
+
+To test on devnet (required for later):
+1. In `contracts`: `clarinet integrate`
+2. While devnet is loading: install [Leather](https://chromewebstore.google.com/detail/leather/ldinpeekobnhjjdofggfgjlcehhmanlj) on Chrome
+3. In Leather: press use existing key, then paste the mnemonic from `accounts.wallet_1` in `settings/Devnet.toml`
+4. Set a password for Leather
+5. Go to `localhost:8000`
+6. Look through the transactions (bottom right) and find the contract deploy of hello-stacks
+    1. If you can't find it, press 'View All Recent Transactions' at the bottom
+7. Copy the contract field of the transaction
+8. Press 'Sandbox' (top right)
+9. Press 'Connect Stacks Wallet'
+10. Connect with the imported account
+11. Press the function symbol (call functions)
+12. Paste in the copied contract field into the top input box
+13. Press 'Get Contract'
+14. Press 'write-message'
+15. Add a message and press 'call function'
+16. Confirm the transaction on the wallet
+17. Go back to the 'Transactions' page and look for the confirmed transaction
+
+##### `./frontend/src/App.tsx`
+
+This is our basic React frontend that interacts with the smart contracts. It allows a user to send and receive a message.
+
+To test:
+1. Make sure devnet is running (follow previous instructions)
+2. In a different terminal: `cd frontend`
+3. `yarn install`
+4. `yarn dev`
+5. Go to `localhost:5173`
+6. Submit a message using the UI
+7. Go back to the block explorer (`localhost:8000`) and wait for the message to be processed by the chain
+8. Copy the transaction ID and paste it back into the frontend (`localhost:5173`) to retrieve the message
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,144 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import {
+  AppConfig,
+  UserSession,
+  showConnect,
+  openContractCall,
+} from "@stacks/connect";
+import { StacksMocknet } from "@stacks/network";
+import { stringUtf8CV } from "@stacks/transactions";
 
 function App() {
+  // State variable for the message to be submitted
+  const [message, setMessage] = useState("");
+
+  // State variable for the transaction id to be retrieved
+  const [transactionId, setTransactionId] = useState("");
+
+  // State variable for the current message to be displayed
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [userData, setUserData] = useState(undefined);
+
+  const appConfig = new AppConfig(["store_write"]);
+  const userSession = new UserSession({ appConfig });
+
+  const appDetails = {
+    name: "Hello Stacks",
+    icon: "https://freesvg.org/img/1541103084.png",
+  };
+
+  // Connects to the Stacks (Leather) wallet
+  const connectWallet = () => {
+    showConnect({
+      appDetails,
+      onFinish: () => window.location.reload(),
+      userSession,
+    });
+  };
+
+  // Retrieves the user data when the user logs into the page
+  useEffect(() => {
+    if (userSession.isSignInPending()) {
+      userSession.handlePendingSignIn().then((userData) => {
+        setUserData(userData);
+      });
+    } else if (userSession.isUserSignedIn()) {
+      setUserData(userSession.loadUserData());
+    }
+  }, []);
+
+  // Changes the message based on user input
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  // Submits the message to the contract
+  const submitMessage = async (e) => {
+    e.preventDefault();
+  
+    const network = new StacksMocknet();
+  
+    const options = {
+      contractAddress: "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM",
+      contractName: "hello-stacks",
+      functionName: "write-message",
+      functionArgs: [stringUtf8CV(message)],
+      network,
+      appDetails,
+      onFinish: ({ txId }) => console.log(txId),
+    };
+  
+    await openContractCall(options);
+  };
+
+  const handleTransactionChange = (e) => {
+    setTransactionId(e.target.value);
+  };
+
+  // Retrieves the most recently published message from the chain
+  const retrieveMessage = async () => {
+    const retrievedMessage = await fetch(
+      "http://localhost:3999/extended/v1/tx/events?" +
+        new URLSearchParams({
+          tx_id: transactionId,
+        })
+    );
+    const responseJson = await retrievedMessage.json();
+    setCurrentMessage(responseJson.events[0].contract_log.value.repr);
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl">Hello Stacks</h1>
+    <div className="flex flex-col justify-center items-center w-screen h-screen gap-8">
+      {
+        !userData && (
+          <button
+            className="p-4 bg-indigo-500 rounded text-white"
+            onClick={connectWallet}
+          >
+            Connect Wallet
+          </button>
+        )
+      }
+      <h1 className="text-6xl font-black">Hello Stacks</h1>
+      {
+        userData && (
+          <div className="flex gap-4">
+            <input
+              className="p-4 border border-indigo-500 rounded"
+              placeholder="Write message here..."
+              onChange={handleMessageChange}
+              value={message}
+            />
+            <button
+              className="p-4 bg-indigo-500 rounded text-white"
+              onClick={submitMessage}
+            >
+              Submit New Message
+            </button>
+          </div>
+        )
+      }
+      <div className="flex gap-4">
+        <input
+          className="p-4 border border-indigo-500 rounded"
+          placeholder="Paste transaction ID to look up message"
+          onChange={handleTransactionChange}
+          value={transactionId}
+        />
+        <button
+          className="p-4 bg-indigo-500 rounded text-white"
+          onClick={retrieveMessage}
+        >
+          Retrieve Message
+        </button>
+      </div>
+      {currentMessage.length > 0 ? (
+        <p className="text-2xl">{currentMessage}</p>
+      ) : (
+        ""
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
